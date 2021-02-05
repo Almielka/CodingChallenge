a56dd629378e548ba4f0c12ea1eff5c7ffd2f404
Almielka <anna.s.almielka@gmail.com>
2021-02-05 17:16:32 +0100

    Solution Day 5: Simplify Path

---
+++ src/test/java/leetCodingChallenge/year2021/february/week1/Day05Test.java
@@ -0,0 +1,49 @@
+package leetCodingChallenge.year2021.february.week1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+class Day05Test {
+
+    private Day05 day;
+
+    @BeforeEach
+    void setUp() {
+        day = new Day05();
+    }
+
+    @Test
+    void testSimplifyPath() {
+        assertThat(day.simplifyPath("/home/"), is(equalTo("/home")));
+    }
+
+    @Test
+    void testSimplifyPath2() {
+        assertThat(day.simplifyPath("/../"), is(equalTo("/")));
+    }
+
+    @Test
+    void testSimplifyPath3() {
+        assertThat(day.simplifyPath("/home//foo/"), is(equalTo("/home/foo")));
+    }
+
+    @Test
+    void testSimplifyPath4() {
+        assertThat(day.simplifyPath("/a/./b/../../c/"), is(equalTo("/c")));
+    }
+
+    @Test
+    void testSimplifyPath5() {
+        assertThat(day.simplifyPath("/..."), is(equalTo("/...")));
+    }
+
+    @Test
+    void testSimplifyPath6() {
+        assertThat(day.simplifyPath("/..hidden"), is(equalTo("/..hidden")));
+    }
+
+}
