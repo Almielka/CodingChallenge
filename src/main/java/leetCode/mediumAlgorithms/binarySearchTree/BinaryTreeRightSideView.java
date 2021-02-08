6eaefd15429dcd790f3652171e69540551d82a68
Almielka <anna.s.almielka@gmail.com>
2021-02-08 10:41:56 +0100

    Added solution 199.Binary Tree Right Side View

---
+++ src/main/java/leetCode/mediumAlgorithms/binarySearchTree/BinaryTreeRightSideView.java
@@ -0,0 +1,48 @@
+package leetCode.mediumAlgorithms.binarySearchTree;
+
+import leetCode.shared.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Anna S. Almielka
+ */
+
+public class BinaryTreeRightSideView {
+
+    /**
+     * 199.Binary Tree Right Side View
+     *
+     * Given a binary tree, imagine yourself standing on the right side of it,
+     * return the values of the nodes you can see ordered from top to bottom.
+     *
+     * @param root a binary tree
+     * @return the right side of it ordered from top to bottom
+     */
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        List<TreeNode> currentRoots = new ArrayList<>();
+        currentRoots.add(root);
+        while (!currentRoots.isEmpty()) {
+            List<TreeNode> tmpList = new ArrayList<>();
+            result.add(currentRoots.get(0).val);
+            for (TreeNode currentRoot : currentRoots) {
+                if (currentRoot.right != null) {
+                    tmpList.add(currentRoot.right);
+                }
+                if (currentRoot.left != null) {
+                    tmpList.add(currentRoot.left);
+                }
+            }
+            currentRoots = tmpList;
+
+        }
+        return result;
+    }
+
+}
+
---
+++ src/test/java/leetCode/mediumAlgorithms/binarySearchTree/BinaryTreeRightSideViewTest.java
@@ -0,0 +1,61 @@
+package leetCode.mediumAlgorithms.binarySearchTree;
+
+
+import leetCode.shared.TreeNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+class BinaryTreeRightSideViewTest {
+
+    private BinaryTreeRightSideView day;
+
+    @BeforeEach
+    void setUp() {
+        day = new BinaryTreeRightSideView();
+    }
+
+    @Test
+    void testRightSideView() {
+        // Input: [1,2,3,null,5,null,4]
+        //          1
+        //       /    \
+        //      2       3
+        //       \        \
+        //         5        4
+        // Output: [1, 3, 4]
+        TreeNode root = new TreeNode(1, new TreeNode(2, null, new TreeNode(5)), new TreeNode(3, null, new TreeNode(4)));
+        List<Integer> result = Arrays.asList(1, 3, 4);
+        assertThat(day.rightSideView(root), is(equalTo(result)));
+    }
+
+    @Test
+    void testRightSideView2() {
+        // Input: []
+        // Output: []
+        TreeNode root = new TreeNode(1, new TreeNode(2), null);
+        List<Integer> result = Arrays.asList(1, 2);
+        assertThat(day.rightSideView(root), is(equalTo(result)));
+    }
+
+    @Test
+    void testRightSideView3() {
+        // Input: [1,2,3,4]
+        //          1
+        //       /    \
+        //      2       3
+        //     /
+        //    4
+        // Output: [1,3,4]
+        TreeNode root = new TreeNode(1, new TreeNode(2, new TreeNode(4), null), new TreeNode(3));
+        List<Integer> result = Arrays.asList(1, 3, 4);
+        assertThat(day.rightSideView(root), is(equalTo(result)));
+    }
+
+}
