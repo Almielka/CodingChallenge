f0a75d0784f295d3776bd133c797c953999d5d3f
Almielka <anna.s.almielka@gmail.com>
2021-02-12 19:24:09 +0100

    Solution Day 12: Number of Steps to Reduce a Number to Zero

---
+++ src/test/java/leetCodingChallenge/year2021/february/week2/Day12Test.java
@@ -0,0 +1,34 @@
+package leetCodingChallenge.year2021.february.week2;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+class Day12Test {
+
+    private Day12 day;
+
+    @BeforeEach
+    void setUp() {
+        day = new Day12();
+    }
+
+    @Test
+    void testNumberOfSteps() {
+        assertThat(day.numberOfSteps(14), is(equalTo(6)));
+    }
+
+    @Test
+    void testNumberOfSteps2() {
+        assertThat(day.numberOfSteps(8), is(equalTo(4)));
+    }
+
+    @Test
+    void testNumberOfSteps3() {
+        assertThat(day.numberOfSteps(123), is(equalTo(12)));
+    }
+
+}
