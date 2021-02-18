7f862a0f6f42d298f2079b8a2205430782d3ab8d
Almielka <anna.s.almielka@gmail.com>
2021-02-18 16:09:14 +0100

    Solution Day 18: Arithmetic Slices

---
+++ src/test/java/leetCodingChallenge/year2021/february/week3/Day18Test.java
@@ -0,0 +1,39 @@
+package leetCodingChallenge.year2021.february.week3;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+class Day18Test {
+
+    private Day18 day;
+
+    @BeforeEach
+    void setUp() {
+        day = new Day18();
+    }
+
+    @Test
+    void testNumberOfArithmeticSlices() {
+        // A = [1, 2, 3, 4]
+        // return: 3, for 3 arithmetic slices in A: [1, 2, 3], [2, 3, 4] and [1, 2, 3, 4] itself.
+        int[] arr = {1, 2, 3, 4};
+        assertThat(day.numberOfArithmeticSlices(arr), is(equalTo(3)));
+    }
+
+    @Test
+    void testNumberOfArithmeticSlices2() {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        assertThat(day.numberOfArithmeticSlices(arr), is(equalTo(10)));
+    }
+
+    @Test
+    void testNumberOfArithmeticSlices3() {
+        int[] arr = {1, 2, 3, 8, 9, 10};
+        assertThat(day.numberOfArithmeticSlices(arr), is(equalTo(2)));
+    }
+
+}
