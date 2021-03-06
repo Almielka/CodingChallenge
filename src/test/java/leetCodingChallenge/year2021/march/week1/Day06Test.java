847157c47e37d2908b2beda4c0ddafe733cb033d
Almielka <anna.s.almielka@gmail.com>
2021-03-06 19:10:10 +0100

    Solution Day 6: Short Encoding of Words

---
+++ src/test/java/leetCodingChallenge/year2021/march/week1/Day06Test.java
@@ -0,0 +1,52 @@
+package leetCodingChallenge.year2021.march.week1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+class Day06Test {
+
+    private Day06 day;
+
+    @BeforeEach
+    void setUp() {
+        day = new Day06();
+    }
+
+    @Test
+    void testMinimumLengthEncoding() {
+        // Input: words = ["time", "me", "bell"]
+        // Output: 10
+        // Explanation: A valid encoding would be s = "time#bell#" and indices = [0, 2, 5].
+        // words[0] = "time", the substring of s starting from indices[0] = 0 to the next '#' is underlined in "time#bell#"
+        // words[1] = "me", the substring of s starting from indices[1] = 2 to the next '#' is underlined in "time#bell#"
+        // words[2] = "bell", the substring of s starting from indices[2] = 5 to the next '#' is underlined in "time#bell#"
+        String[] words = {"time", "me", "bell"};
+        assertThat(day.minimumLengthEncoding(words), is(equalTo(10)));
+    }
+
+    @Test
+    void testMinimumLengthEncoding2() {
+        // Input: words = ["t"]
+        // Output: 2
+        // Explanation: A valid encoding would be s = "t#" and indices = [0].
+        String[] words = {"t"};
+        assertThat(day.minimumLengthEncoding(words), is(equalTo(2)));
+    }
+
+    @Test
+    void testMinimumLengthEncoding3() {
+        String[] words = {"feipyxx", "e"};
+        assertThat(day.minimumLengthEncoding(words), is(equalTo(10)));
+    }
+
+    @Test
+    void testMinimumLengthEncoding4() {
+        String[] words = {"me", "time"};
+        assertThat(day.minimumLengthEncoding(words), is(equalTo(5)));
+    }
+
+}
