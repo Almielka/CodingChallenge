577a2df78c60cee0b901d9cc8f07d293c036053a
Almielka <anna.s.almielka@gmail.com>
2021-03-08 17:27:30 +0100

    Solution Day 8: Remove Palindromic Subsequences

---
+++ src/test/java/leetCodingChallenge/year2021/march/week2/Day08Test.java
@@ -0,0 +1,52 @@
+package leetCodingChallenge.year2021.march.week2;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+class Day08Test {
+
+    private Day08 day;
+
+    @BeforeEach
+    void setUp() {
+        day = new Day08();
+    }
+
+    @Test
+    void testMinimumLengthEncoding() {
+        // Input: s = "ababa"
+        // Output: 1
+        // Explanation: String is already palindrome
+        assertThat(day.removePalindromeSub("ababa"), is(equalTo(1)));
+    }
+
+    @Test
+    void testMinimumLengthEncoding2() {
+        // Input: s = "abb"
+        // Output: 2
+        // Explanation: "abb" -> "bb" -> "".
+        // Remove palindromic subsequence "a" then "bb".
+        assertThat(day.removePalindromeSub("abb"), is(equalTo(2)));
+    }
+
+    @Test
+    void testMinimumLengthEncoding3() {
+        // Input: s = "baabb"
+        // Output: 2
+        // Explanation: "baabb" -> "b" -> "".
+        // Remove palindromic subsequence "baab" then "b".
+        assertThat(day.removePalindromeSub("baabb"), is(equalTo(2)));
+    }
+
+    @Test
+    void testMinimumLengthEncoding4() {
+        // Input: s = ""
+        // Output: 0
+        assertThat(day.removePalindromeSub(""), is(equalTo(0)));
+    }
+
+}
