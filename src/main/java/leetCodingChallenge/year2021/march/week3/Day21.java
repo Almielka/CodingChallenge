f9e68dcb3257cf91198ba50e15ac9c35b70a91a4
Almielka <anna.s.almielka@gmail.com>
2021-03-21 12:19:40 +0100

    Solution Day 21: Reordered Power of 2

---
+++ src/main/java/leetCodingChallenge/year2021/march/week3/Day21.java
@@ -0,0 +1,43 @@
+package leetCodingChallenge.year2021.march.week3;
+
+import java.util.Arrays;
+
+/**
+ * @author Anna S. Almielka
+ * 21.03.2021
+ */
+
+public class Day21 {
+
+    /**
+     * Starting with a positive integer N, we reorder the digits in any order
+     * (including the original order) such that the leading digit is not zero.
+     * Return true if and only if we can do this in a way such that the resulting number is a power of 2.
+     * Note:
+     * 1 <= N <= 10^9
+     *
+     * @param N a positive integer
+     * @return true if and only if we can do this in a way such that the resulting number is a power of 2
+     */
+    // Runtime: 1 ms
+    // Memory Usage: 36.1 MB
+    public boolean reorderedPowerOf2(int N) {
+        int[] digits = countDigits(N);
+        for (int i = 0; i < 30; ++i)
+            // (1 << i) power of 2 from 0 to 29 bit (2^30 > 10^9)
+            if (Arrays.equals(digits, countDigits(1 << i))) {
+                return true;
+            }
+        return false;
+    }
+
+    public int[] countDigits(int N) {
+        int[] digits = new int[10];
+        while (N > 0) {
+            digits[N % 10]++;
+            N /= 10;
+        }
+        return digits;
+    }
+
+}
---
+++ src/test/java/leetCodingChallenge/year2021/march/week3/Day21Test.java
@@ -0,0 +1,54 @@
+package leetCodingChallenge.year2021.march.week3;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+class Day21Test {
+
+    private Day21 day;
+
+    @BeforeEach
+    void setUp() {
+        day = new Day21();
+    }
+
+    @Test
+    void testReorderedPowerOf2() {
+        assertThat(day.reorderedPowerOf2(1), is(equalTo(true)));
+    }
+
+    @Test
+    void testReorderedPowerOf2test2() {
+        assertThat(day.reorderedPowerOf2(10), is(equalTo(false)));
+    }
+
+    @Test
+    void testReorderedPowerOf2test3() {
+        assertThat(day.reorderedPowerOf2(16), is(equalTo(true)));
+    }
+
+    @Test
+    void testReorderedPowerOf2test4() {
+        assertThat(day.reorderedPowerOf2(24), is(equalTo(false)));
+    }
+
+    @Test
+    void testReorderedPowerOf2test5() {
+        assertThat(day.reorderedPowerOf2(46), is(equalTo(true)));
+    }
+
+    @Test
+    void testReorderedPowerOf2test6() {
+        assertThat(day.reorderedPowerOf2((int) Math.pow(2, 29)), is(equalTo(true)));
+    }
+
+    @Test
+    void testReorderedPowerOf2test7() {
+        assertThat(day.reorderedPowerOf2((int) Math.pow(10, 9)), is(equalTo(false)));
+    }
+
+}
